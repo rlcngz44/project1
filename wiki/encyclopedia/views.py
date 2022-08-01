02bb391efd53e18a1bf2622c2d2f2fe32ca10849
rlcngz <erolcengiz@gmail.com>
2022-08-01 15:31:23 +0200

    Create entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
+from django.http import HttpResponse
 from . import util
 
 
@@ -8,3 +9,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry_md = util.get_entry(title)
+
+    if entry_md:
+        # Title exists, convert md to HTML and return rendered template
+        entry_HTML = Markdown().convert(entry_md)
+        print(entry_HTML)
+        return render(request, "encyclopedia/entry.html", {"title": title, "entry": entry_HTML})
+    else:
+        return HttpResponse("No entry, please check your entry!")
